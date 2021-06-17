2ea70b40a0b09063afc1f1fae58acf89e7640e63
Comelli <ivansrib@gmail.com>
2021-06-17 02:41:46 -0300

    Primeiro Ajuste

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exerc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salarioReajustado;
+
+            Console.WriteLine("Entre com o valor do salario");
+            string salario = Console.ReadLine();
+
+            double valorSalario = Convert.ToDouble(salario);
+
+            Console.WriteLine("Entre com o valor do reajuste");
+            string reajuste = Console.ReadLine();
+
+            double valReajuste = Convert.ToDouble(reajuste);
+
+            salarioReajustado = valorSalario + ((valorSalario / 100) * valReajuste);
+            Console.WriteLine(salarioReajustado);
+
+        }
+    }
+}
